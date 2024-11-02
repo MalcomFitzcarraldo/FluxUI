c0385dbc872f0cff2e519b1f3e320982901ae39a
Malcom Fitzcarraldo <drakep@live.com>
2024-11-02 10:56:00 -0500

    Update finished.py

--- back/finished.py
+++ back/finished.py
@@ -3,6 +3,6 @@ import subprocess
 print(Fore.WHITE + Style.BRIGHT + Back.BLUE + "FluxUI" + Style.RESET_ALL)
 print(Fore.GREEN + "" + Style.RESET_ALL)
 print(Fore.GREEN + "Response Complete." + Style.RESET_ALL)
-print(Fore.WHITE + "Press ENTER for new query." + Style.RESET_ALL)
+print(Fore.YELLOW + Style.BRIGHT + "Press ENTER for new query." + Style.RESET_ALL)
 input()
 subprocess.Popen(['cmd', '/c', 'start', 'python', 'C:/FluxUI/FluxUI.py'])
