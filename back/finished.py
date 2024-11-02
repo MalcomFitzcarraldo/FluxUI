984badc07dfef86f5160e7249bfa34c5c8b6b02e
Malcom Fitzcarraldo <drakep@live.com>
2024-11-02 08:44:49 -0500

    Update finished.py

--- back/finished.py
+++ back/finished.py
@@ -1,10 +1,8 @@
 from colorama import Fore, Style, Back
 import subprocess
-
-print(Fore.CYAN + "FluxUI" + Style.RESET_ALL)
+print(Fore.WHITE + Style.BRIGHT + Back.BLUE + "FluxUI" + Style.RESET_ALL)
 print(Fore.GREEN + "" + Style.RESET_ALL)
 print(Fore.GREEN + "Response Complete." + Style.RESET_ALL)
-print(Back.BLUE)
 print(Fore.WHITE + "Press ENTER for new query." + Style.RESET_ALL)
 input()
 subprocess.Popen(['cmd', '/c', 'start', 'python', 'C:/FluxUI/FluxUI.py'])
