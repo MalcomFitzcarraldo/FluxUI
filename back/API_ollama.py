854ba2dd981354bb912fc0fb796f2233fa099b04
Malcom Fitzcarraldo <drakep@live.com>
2024-11-02 08:42:36 -0500

    Update API_ollama.py

--- back/API_ollama.py
+++ back/API_ollama.py
@@ -2,7 +2,7 @@ from colorama import Fore, Style
 import subprocess
 import time
 from ollama import Client
-print(Fore.CYAN + "FluxUI" + Style.RESET_ALL)
+print(Fore.GREEN + "Signal Clear! " + Style.RESET_ALL)
 print(Fore.GREEN + "" + Style.RESET_ALL)
 print(Style.DIM)
 print(Fore.WHITE)
@@ -11,7 +11,7 @@ file_path = r'C:\FluxUI\temp\prompt_input.txt'
 output_file_path = r'C:\FluxUI\temp\output.txt'
 signal_file_path = r'C:\FluxUI\temp\done.txt'
 
-# Read the content of the input file
+# Read the input file
 with open(file_path, 'r') as file:
     file_content = file.read().strip()
 
@@ -21,15 +21,15 @@ response_stream = client.chat(model='gemma2:9b', messages=[{
     'content': f'{file_content}',
 }], stream=True)
 
-# Open the output file in write mode
+# Open the output
 with open(output_file_path, 'w') as output_file:
-    # Start the scheduler.py script in a new window
+    # Start the scheduler
     subprocess.run([r'C:/FluxUI/back/signal_reset.bat'])
     subprocess.Popen(['cmd', '/c', 'start', 'python', 'C:/FluxUI/back/update_timer.py'])
     
     for chunk in response_stream:
         # Print the chunk to the console
-        print(chunk['message']['content'], end='')
+        # print(chunk['message']['content'], end='')
         # Write the chunk to the output file
         output_file.write(chunk['message']['content'])
         # Ensure content is written to the file immediately
@@ -40,4 +40,5 @@ with open(signal_file_path, 'w') as signal_file: signal_file.write('done')
 time.sleep(2)
 subprocess.Popen(['cmd', '/c', 'start', 'python', 'C:/FluxUI/back/getrich_final.py'])
 subprocess.Popen(['cmd', '/c', 'start', 'python', 'C:/FluxUI/back/finished.py'])
-print(Fore.GREEN + "\nResponse saved to output.txt")
+print(Fore.GREEN + "\nResponse cached to temp/output.txt")
+
