55dbdd9b4c65ab2148ba84073dd88bce757d525c
Malcom Fitzcarraldo <drakep@live.com>
2024-11-05 10:56:56 -0600

    Update API_ollama.py

--- back/API_ollama.py
+++ back/API_ollama.py
@@ -12,8 +12,9 @@ output_file_path = r'C:\FluxUI\temp\output.txt'
 signal_file_path = r'C:\FluxUI\temp\done.txt'
 
 # Read the input file
-with open(file_path, 'r') as file:
+with open(file_path, 'r', encoding='utf-8') as file:
     file_content = file.read().strip()
+
 
 client = Client(host='http://localhost:11434')
 response_stream = client.chat(model='gemma2:9b', messages=[{
@@ -22,7 +23,7 @@ response_stream = client.chat(model='gemma2:9b', messages=[{
 }], stream=True)
 
 # Open the output
-with open(output_file_path, 'w') as output_file:
+with open(output_file_path, 'w', encoding='utf-8') as output_file:
     for chunk in response_stream:
         # Print the chunk to the console
         # print(chunk['message']['content'], end='')
@@ -35,7 +36,9 @@ with open(output_file_path, 'w') as output_file:
 with open(signal_file_path, 'w') as signal_file: signal_file.write('done')
 time.sleep(3)
 subprocess.Popen(['cmd', '/c', 'start', 'python', 'C:/FluxUI/back/getrich_final.py'])
-subprocess.Popen(['cmd', '/c', 'start', 'python', 'C:/FluxUI/back/finished.py'])
+time.sleep(3)
+subprocess.run(["python", "C:/FluxUI/back/finished.py"])
+#subprocess.Popen(['cmd', '/c', 'start', 'python', 'C:/FluxUI/back/finished.py'])
 print(Fore.GREEN + "\nResponse cached to temp/output.txt")
-time.sleep(1)
+
 
