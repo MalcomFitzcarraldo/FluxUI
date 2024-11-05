768149ff7f020f5447a71468c9c6824a39242e47
Malcom Fitzcarraldo <drakep@live.com>
2024-11-05 10:58:27 -0600

    Update FluxUI.py

--- FluxUI.py
+++ FluxUI.py
@@ -1,8 +1,9 @@
-from colorama import Fore, Style
+from colorama import Fore, Style, Back
 import subprocess
-print(Fore.GREEN + "FluxUI" + Style.RESET_ALL)
+import os
+
+print(Fore.WHITE + Style.BRIGHT + Back.BLUE + "FluxUI" + Style.RESET_ALL)
 subprocess.run([r'C:/FluxUI/back/clear_temp.bat'])
-subprocess.run(['cmd', '/c', 'start', 'python', 'C:/FluxUI/back/request_input.py'])
-
-
-
+os.system('cls')
+#subprocess.run(['cmd', '/c', 'start', 'python', 'C:/FluxUI/back/request_input.py'])
+subprocess.run(["python", "C:/FluxUI/back/web/watcher.py"])
